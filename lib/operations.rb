8fc2ed30d2c3f8d2c1aaa28ae7e9b13d320133c9
Devin Arnold <dbarnold321@gmail.com>
2019-05-01 01:16:46 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,7 +1,10 @@
 def unsafe?(speed)
-	if speed > 60 ? true : false
-	elsif speed < 40 ? true : false
-	elsif speed > 60 && speed < 40 ? true : false
+	if speed > 60
+		return true
+	elsif speed < 40
+		return true
+	elsif speed > 60 && speed < 40
+		return true
 end
 end
 
